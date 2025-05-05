bb8f5ba3df47cf8f4bf1381ab18ab491b1c90af5
Anbumozhi53 <anbumozhi132809@gmail.com>
2025-05-05 21:54:34 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,19 @@
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email
+    @NotBlank
+    private String email;
+}
